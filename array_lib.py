0afe798f9e5bdd8d508d1b37c2e56ff3b083837c
Yrii Chernyshov <c300@list.ru>
2026-01-09 19:56:46 +0300

    Доб ф-ю произведения

--- array_lib.py
+++ array_lib.py
@@ -1,8 +1,28 @@
-def sum_1d(arr: list) -> float | None:
+def sum_1d(arr: list) -> float | bool:
+
+
+    if len(arr) == '':
+        return False
 
     summ = 0
     for numb in arr:
         summ += numb
 
     return summ
+
+
+def prod_1d(arr: list) -> float | bool:
+
+    if len(arr) == '':
+        return False
+
+    prod = 0
+
+    for numb in arr:
+        prod = prod * numb
+    return prod
+
+
+
+
 
